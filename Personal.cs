5a65f96b17b2935c66039e8b111eea1a86fa0eb5
mike050908 <117846756+mike050908@users.noreply.github.com>
2022-11-23 18:32:00 +0300

    Add files via upload

---
+++ Personal.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personal : MonoBehaviour
+{
+    public int health = 5;
+
+    public int level = 1;
+
+    public float speed = 1.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health += level;
+        print("Жизней:" + health)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.x += speed * Tine.deltaTime;
+        transform.position = newPosition;
+    }
+}
